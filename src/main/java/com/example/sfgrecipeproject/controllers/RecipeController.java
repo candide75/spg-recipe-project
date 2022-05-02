dae22a257fe7767ff18c4e4002b138ed4cd2247f
candide75 <dealmeida.candido@gmail.com>
2022-05-02 10:13:32 +0200

    Adding "create a recipe"

--- src/main/java/com/example/sfgrecipeproject/controllers/RecipeController.java
+++ src/main/java/com/example/sfgrecipeproject/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package com.example.sfgrecipeproject.controllers;
 
+import com.example.sfgrecipeproject.commands.RecipeCommand;
 import com.example.sfgrecipeproject.services.RecipeService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -26,5 +29,20 @@ public class RecipeController {
     public String showById(Model model, @PathVariable Long id) {
         model.addAttribute("recipe", recipeService.getRecipeById(id));
         return "/recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeForm";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect/recipe/show/" + savedCommand.getId();
     }
 }
